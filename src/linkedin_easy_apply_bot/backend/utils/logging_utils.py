fb20c60c4afe98e6316244a72eb684c3b490126f
Vikash Kothary <kothary.vikash@gmail.com>
2025-07-10 22:46:02 +0100

    feat: Update logging file with iso timestamps

--- src/linkedin_easy_apply_bot/backend/utils/logging_utils.py
+++ src/linkedin_easy_apply_bot/backend/utils/logging_utils.py
@@ -3,33 +3,40 @@ import logging
 from datetime import datetime, timedelta
 
 from linkedin_easy_apply_bot.backend import config
+from linkedin_easy_apply_bot.backend.utils import datetime_utils
+
 
 # Suppress selenium debug logs
 logging.getLogger("selenium").setLevel(logging.INFO)
 
 
-def get_logger(name):
-    log = logging.getLogger(name)
-    dt: str = datetime.strftime(datetime.now(), "%m_%d_%y %H_%M_%S ")
+def get_logger(name: str) -> logging.Logger:
+    logs_dir = 'instances/logs'
+    if not os.path.isdir(logs_dir):
+        os.mkdir(logs_dir)
 
-    if not os.path.isdir("./logs"):
-        os.mkdir("./logs")
-
+    # Logging defaults
+    now: str = datetime_utils.to_datetime_string(datetime_utils.now())
     # TODO need to check if there is a log dir available or not
+    log_file_name = f"{logs_dir}/{now}/applyJobs.log"
     logging.basicConfig(
         level=config.LOG_LEVEL,
-        filename=("./logs/" + str(dt) + "applyJobs.log"),
+        filename=log_file_name,
         filemode="w",
         format="%(asctime)s::%(name)s::%(levelname)s::%(message)s",
-        datefmt="./logs/%d-%b-%y %H:%M:%S",
+        datefmt=f"{logs_dir}/%d-%b-%y %H:%M:%S",
     )
-    log.setLevel(logging.DEBUG)
-    c_handler = logging.StreamHandler()
-    c_handler.setLevel(logging.DEBUG)
-    c_format = logging.Formatter(
+
+    # Logging format
+    log_format = logging.Formatter(
         "%(asctime)s - %(levelname)s - %(message)s", "%H:%M:%S"
     )
-    c_handler.setFormatter(c_format)
-    log.addHandler(c_handler)
+    log_stream = logging.StreamHandler()
+    log_stream.setLevel(logging.DEBUG)
+    log_stream.setFormatter(log_format)
 
-    return log
+    # Logger
+    logger = logging.getLogger(name) 
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(log_stream)
+    return logger
